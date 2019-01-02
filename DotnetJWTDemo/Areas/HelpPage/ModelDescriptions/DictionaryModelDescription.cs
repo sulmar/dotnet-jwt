5db19a145d1f5377eb18cd5db8fc828e43630236
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-01-02 14:11:56 +0100

    Add project files.

---
+++ DotnetJWTDemo/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DotnetJWTDemo.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DotnetJWTDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotnetJWTDemo.WebApiApplication" Language="C#" %>
